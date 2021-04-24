8ed5dbbf6e25e2ac2b89ef5e17d49377b3a5d214
Ben Jarrett <76926244+BenJarrett@users.noreply.github.com>
2021-04-23 22:16:55 -0500

    Initialize get-books

---
+++ src/javascripts/events/domEvents.js
@@ -0,0 +1,75 @@
+import { showAuthors } from '../components/authors';
+import { showBooks } from '../components/books';
+import addAuthorForm from '../components/forms/addAuthorForm';
+import addBookForm from '../components/forms/addBookForm';
+import { createAuthors, deleteAuthor } from '../helpers/data/authorData';
+import { createBook, deleteBook } from '../helpers/data/bookData';
+
+const domEvents = () => {
+  document.querySelector('body').addEventListener('click', (e) => {
+    // CLICK EVENT FOR DELETING A BOOK
+    if (e.target.id.includes('delete-book')) {
+      if (window.confirm('Want to delete?')) {
+        // pull the firebaseKey off the button
+        const firebaseKey = e.target.id.split('--')[1];
+        deleteBook(firebaseKey).then((booksArray) => showBooks(booksArray));
+      }
+    }
+
+    // CLICK EVENT FOR SHOWING FORM FOR ADDING A BOOK
+    if (e.target.id.includes('add-book-btn')) {
+      console.warn('CLICKED ADD BOOK BUTTON', e.target.id);
+      addBookForm();
+    }
+
+    // CLICK EVENT FOR SUBMITTING FORM FOR ADDING A BOOK
+    if (e.target.id.includes('submit-book')) {
+      e.preventDefault();
+      const bookObject = {
+        title: document.querySelector('#title').value,
+        image: document.querySelector('#image').value,
+        price: document.querySelector('#price').value,
+        sale: document.querySelector('#sale').checked,
+        author_id: document.querySelector('#author').value,
+      };
+      createBook(bookObject).then((booksArray) => showBooks(booksArray));
+    }
+    // CLICK EVENT FOR SHOWING MODAL FORM FOR ADDING A BOOK
+    if (e.target.id.includes('edit-book-btn')) {
+      console.warn('CLICKED EDIT BOOK', e.target.id);
+    }
+
+    // CLICK EVENT FOR EDITING A BOOK
+    if (e.target.id.includes('update-book')) {
+      console.warn('CLICKED EDIT BOOK', e.target.id);
+    }
+
+    // ADD CLICK EVENT FOR DELETING AN AUTHOR
+    if (e.target.id.includes('delete-author')) {
+      if (window.confirm('Want to delete?')) {
+        const firebaseKey = e.target.id.split('--')[1];
+        deleteAuthor(firebaseKey).then((authorsArray) => showAuthors(authorsArray));
+      }
+    }
+    // ADD CLICK EVENT FOR SHOWING FORM FOR ADDING AN AUTHOR
+    if (e.target.id.includes('add-author-btn')) {
+      console.warn('Clicked Add Author', e.target.id);
+      addAuthorForm();
+    }
+    // ADD CLICK EVENT FOR SUBMITTING FORM FOR ADDING AN AUTHOR
+    if (e.target.id.includes('submit-author')) {
+      e.preventDefault();
+      console.warn('clicked', e.target.id);
+      const authorObject = {
+        first_name: document.querySelector('#first').value,
+        last_name: document.querySelector('#last').value,
+        email: document.querySelector('#email').value,
+        favorite: document.querySelector('#favorite').checked,
+      };
+      createAuthors(authorObject).then((authorsArray) => showAuthors(authorsArray));
+    }
+    // ADD CLICK EVENT FOR EDITING AN AUTHOR
+  });
+};
+
+export default domEvents;
